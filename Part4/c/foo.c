0b8eb41b10b94446578eb9bd2044e2c618d14183
kentare <kentat@stud.ntnu.no>
2019-01-11 14:07:52 +0100

    C, GO and Python code OK

--- Part4/c/foo.c
+++ Part4/c/foo.c
@@ -6,23 +6,33 @@ int i = 0;
 // Note the return type: void*
 void* incrementingThreadFunction(){
     // TODO: increment i 1_000_000 times
+    int a;
+    for( a = 0; a < 1000000; a = a+1 ){
+            i++;
+    }
     return NULL;
 }
 
 void* decrementingThreadFunction(){
     // TODO: decrement i 1_000_000 times
+    int a;
+    for( a = 0; a < 1000000; a = a+1 ){
+            i--;
+    }
     return NULL;
 }
 
 
 int main(){
     // TODO: declare incrementingThread and decrementingThread (hint: google pthread_create)
+    pthread_t incrementingThread;
+    pthread_t decrementingThread;
     pthread_create(&incrementingThread, NULL, incrementingThreadFunction, NULL);
     pthread_create(&decrementingThread, NULL, decrementingThreadFunction, NULL);
-    
+
     pthread_join(incrementingThread, NULL);
     pthread_join(decrementingThread, NULL);
-    
+
     printf("The magic number is: %d\n", i);
     return 0;
 }
--- Part4/go/foo.go
+++ Part4/go/foo.go
@@ -3,29 +3,39 @@
 package main
 
 import (
-    . "fmt"
-    "runtime"
-    "time"
+	. "fmt"
+	"runtime"
+	"time"
 )
 
 var i = 0
 
 func incrementing() {
-    //TODO: increment i 1000000 times
+	//TODO: increment i 1000000 times
+	for a := 0; a < 100000; a++ {
+		i++
+	}
 }
 
 func decrementing() {
-    //TODO: decrement i 1000000 times
+	//TODO: decrement i 1000000 times
+	for b := 0; b < 100000; b++ {
+		i--
+	}
 }
 
 func main() {
-    runtime.GOMAXPROCS(runtime.NumCPU())    // I guess this is a hint to what GOMAXPROCS does...
-	                                    // Try doing the exercise both with and without it!
+	runtime.GOMAXPROCS(runtime.NumCPU()) // I guess this is a hint to what GOMAXPROCS does...
+	// Try doing the exercise both with and without it!
+	// Kent: GOMAXPROCS sets the maximum number of CPUs that can be executing simultaneously and returns the previous setting. If n < 1, it does not change the current setting.
+	// The number of logical CPUs on the local machine can be queried with the NumCPU.
 
-    // TODO: Spawn both functions as goroutines
-	
-    // We have no way to wait for the completion of a goroutine (without additional syncronization of some sort)
-    // We'll come back to using channels in Exercise 2. For now: Sleep.
-    time.Sleep(100*time.Millisecond)
-    Println("The magic number is:", i)
+	// TODO: Spawn both functions as goroutines
+	go incrementing()
+	go decrementing()
+
+	// We have no way to wait for the completion of a goroutine (without additional syncronization of some sort)
+	// We'll come back to using channels in Exercise 2. For now: Sleep.
+	time.Sleep(100 * time.Millisecond)
+	Println("The magic number is:", i)
 }
